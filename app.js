455ae256054cf3af8383654b0e6c832e798505f8
nkidmano <nobudy55@gmail.com>
2019-02-13 21:24:11 +0700

    actaully work

--- app.js
+++ app.js
@@ -5,52 +5,27 @@ const axios = require('axios');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
 async function fetchGames() {
-  console.log('wtf');
   const { data: ids } = await axios.get(
     'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
   );
 
   const { apps } = ids.applist;
 
-  for (let i = 0; i < 5; i++) {
-    const { data } = await axios.get(
+  for (let i = 0; i < 10; i++) {
+    const { data: games } = await axios.get(
       `https://store.steampowered.com/api/appdetails?appids=${apps[i]['appid']}`
     );
 
-    const obj = data[Object.keys(data)[0]];
-    if (obj['success']) {
-      console.log(data);
+    const result = data[Object.keys(data)[0]];
+    if (result['success']) {
+      console.log(games);
     }
   }
 }
 
-app.get('/', async (req, res) => {
-  res.send('wtf');
-  await fetchGames();
-});
-
-// axios
-//   .get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
-//   .then(({ data }) => {
-//     const { apps } = data.applist;
-//     for (let i = 0; i < 10; i++) {
-//       axios
-//         .get(
-//           `https://store.steampowered.com/api/appdetails?appids=${
-//             apps[i]['appid']
-//           }`
-//         )
-//         .then(({ data }) => {
-//           const obj = data[Object.keys(data)[0]];
-//           if (obj['success']) {
-//             console.log(data);
-//           }
-//         });
-//     }
-//   });
+fetchGames();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
